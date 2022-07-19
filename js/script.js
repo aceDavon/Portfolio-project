562d0001a73c79e74098c80c3e85d0d633d7d4a8
Surafel Getachew <suragetch@gmail.com>
2022-07-19 12:55:36 +0300

    Link the menu items with the page sections

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const menu = document.getElementById('topMenu');
 const Xclose = document.getElementById('close');
 const lists = document.querySelectorAll('#topMenu li');
 const list = Array.from(lists);
-console.log(Xclose);
 
 [hamburger, Xclose, ...list].forEach((Element) => {
   Element.addEventListener('click', () => {
